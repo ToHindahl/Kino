dbdae0080b324e50047feba16926db60db63d306
SHueper <sebastian.hueper@nexgo.de>
2025-03-04 15:06:12 +0100

    Update CommandListener.java

--- DB/src/main/java/de/fhdw/Kino/DB/listener/CommandListener.java
+++ DB/src/main/java/de/fhdw/Kino/DB/listener/CommandListener.java
@@ -17,7 +17,6 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.LinkedHashMap;
-import java.util.List;
 
 @Slf4j
 @Component
